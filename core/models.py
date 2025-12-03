51c5eaa3a489febefb442dc756d9ce79b8e9389e
MohitFD <mohit@fixingdots.in>
2025-12-03 11:17:51 +0530

    Add ChatConversation model for user conversations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# models.py mein add karo
+from django.db import models
+from django.contrib.auth.models import User
+
+class ChatConversation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    employee_id = models.CharField(max_length=50)  # ya session se
+    conv_id = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=200)
+    messages = models.JSONField()
+    timestamp = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.employee_id} - {self.title}"
